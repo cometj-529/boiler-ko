bbd8f043e835a52c379f3898a062de29e041166d
wjdgotjd529 <wjdgotjd529@naver.com>
2022-06-23 11:11:24 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const express = require('express');
+const app = express();
+const port = 3000;
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://wjdgotjd:Baby10180529@haesung.zxosqis.mongodb.net/?retryWrites=true&w=majority', {
+}).then(() => console.log('MongoDB Connected...'))
+ .catch(err => console.log(err))
+
+
+app.get('/', (req, res) => {
+  res.send('HelloWorld!');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
