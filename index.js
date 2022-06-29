de8b34b8c31094f8f03dd9ba18862c280917926b
wjdgotjd529 <wjdgotjd529@naver.com>
2022-06-29 20:55:49 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -3,14 +3,35 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-mongoose.connect('mongodb+srv://wjdgotjd:Baby10180529@haesung.zxosqis.mongodb.net/?retryWrites=true&w=majority', {
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const config = require('./config/key');
+
+const {User} = require("./models/User");
+
+mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDB Connected...'))
  .catch(err => console.log(err))
 
-
 app.get('/', (req, res) => {
-  res.send('HelloWorld!');
+  res.send('Hello World!');
+})
+
+app.post('/register', (req,res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err});
+    return res.status(200).json({
+      success: true
+    })
+  });
+
+  console.log(req.body);
+
 })
 
 app.listen(port, () => {
